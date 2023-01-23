a52c0dd1c93e983187cabbe9d74c9665ff88c1c0
Mehrdad Yazdani <myazdani@gmail.com>
2023-01-22 17:14:55 -0800

    added T learner uplift model

--- src/models.py
+++ src/models.py
@@ -76,3 +76,90 @@ class BinaryUplift(pl.LightningModule):
         parser.add_argument("--hidden_dim", type=int, default=128)
         parser.add_argument("--learning_rate", type=float, default=1e-3)
         return parser
+    
+    
+class UpliftMLP(pl.LightningModule):
+    """PyTorch Lightning implementation of a MLP
+    Args:
+        hidden_dim (int, optional): dimension of hidden layer (default: ``128``).
+        learning_rate (float, optional): optimizer learning rate (default: ``1e-3``).
+    """
+    @staticmethod
+    def fetch_model(input_dim, output_dim, hidden_dim, num_hidden_layers = 1):
+        layers = []
+        for i in range(num_hidden_layers):
+            layers.append(nn.Linear(input_dim if i == 0 else hidden_dim, hidden_dim))
+            layers.append(nn.ReLU())
+        layers.append(nn.Linear(hidden_dim, output_dim))
+        model = nn.Sequential(*layers)
+        return model 
+
+    def __init__(self, input_dim: int, output_dim: int,
+                 hidden_dim: int = 128, num_hidden_layers: int = 1, 
+                 learning_rate: float = 1e-3, **kwargs: Any) -> None:
+        super().__init__()
+        self.save_hyperparameters()
+        
+        self.model_t = UpliftMLP.fetch_model(self.hparams.input_dim, 
+                                              self.hparams.output_dim,
+                                              self.hparams.hidden_dim,
+                                              self.hparams.num_hidden_layers
+                                             )
+        
+        self.model_c = UpliftMLP.fetch_model(self.hparams.input_dim, 
+                                      self.hparams.output_dim,
+                                      self.hparams.hidden_dim,
+                                      self.hparams.num_hidden_layers
+                                     )
+
+
+        self.criterion = torch.nn.BCEWithLogitsLoss()
+        
+    def forward(self, x: Tensor) -> Tensor:
+        out = x.view(x.size(0), -1)
+        out_t = self.model_t(out)
+        out_c = self.model_c(out)
+        return out_t - out_c
+
+    def shared_step(self, batch: Any, batch_idx: int, step: str) -> Tensor:
+        x, y, t = batch
+        t_indx, c_indx = torch.where(t==1)[0], torch.where(t==0)[0]
+        x_t, x_c = x[t_indx,:], x[c_indx,:]
+        y_t, y_c = y[t_indx], y[c_indx]
+        
+        y_t_pred = self.model_t(x_t)
+        y_c_pred = self.model_c(x_c)
+        
+        loss_t = self.criterion(y_t_pred.squeeze(), y_t)
+        loss_c = self.criterion(y_c_pred.squeeze(), y_c)
+        loss = loss_t + loss_c
+
+        if step == "train":
+            self.log("train_loss", loss)
+        elif step == "val":
+            self.log("val_loss", loss)
+        elif step == "test":
+            self.log("test_loss", loss)
+        else:
+            raise ValueError(f"Step {step} is not recognized. Must be 'train', 'val', or 'test'.")
+
+        return loss
+
+    def training_step(self, batch: Any, batch_idx: int) -> Tensor:
+        return self.shared_step(batch, batch_idx, "train")
+
+    def validation_step(self, batch: Any, batch_idx: int) -> None:
+        self.shared_step(batch, batch_idx, "val")
+
+    def test_step(self, batch: Any, batch_idx: int) -> None:
+        self.shared_step(batch, batch_idx, "test")
+
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), lr=self.hparams.learning_rate)
+
+    @staticmethod
+    def add_model_specific_args(parent_parser) -> ArgumentParser:
+        parser = ArgumentParser(parents=[parent_parser], add_help=False)
+        parser.add_argument("--hidden_dim", type=int, default=128)
+        parser.add_argument("--learning_rate", type=float, default=1e-3)
+        return parser    
